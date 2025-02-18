1b07d46e33f92b59f72365e61c775c90de3dbe9f
R-S-A-N-J-A-Y <sanjay638291@gmail.com>
2025-02-18 20:26:30 +0530

    feat: connect MongoDB, create database, and add initial data entry

--- index.js
+++ index.js
@@ -1,8 +1,34 @@
-const express = require('express');
-const app = express();
+const mongoose = require('mongoose');
 
-app.get("/", (req, res) => {
-    res.send("HI");
-})
+mongoose.connect('mongodb://localhost:27017/gamesHub')
+  .then( () => console.log("Connected to the mongodb server...") )
+  .catch( (err) => console.log( "Error Connecting to the mongodb server ", err ) );
 
-app.listen(3000, () => console.log("Listening to the Port 3000..."));
+const schema = mongoose.Schema( {
+    name: String,
+    imageSrc: String,
+    date: Date,
+    ratings: Number,
+    likes: Number,
+    genres: [String],
+    platforms: [String],
+} );
+
+const Games = mongoose.model( 'Games', schema );
+
+
+async function putData () {
+    const games = new Games( {
+        name: "Marvel's Spider-man Remastered",
+        imageSrc : "/GamePreviews/Spiderman-Rem.jpg",
+        likes: 9.2,
+        genres: ["action", "adventure", "stealth", "superhero"],
+        Date: 12/7/2022,
+        platforms: ["ps5", "pc"]
+    } )
+    
+    const result = await games.save();
+    console.log(result);
+}
+
+putData();
