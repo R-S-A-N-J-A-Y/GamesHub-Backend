3904e38515686c65e3cabd1f1c2eda833bbb87c0
R-S-A-N-J-A-Y <sanjay638291@gmail.com>
2025-03-13 21:19:16 +0530

    feat: implement platform-based game querying from database

--- backend/Routes/RouteManger.js
+++ backend/Routes/RouteManger.js
@@ -2,7 +2,8 @@ const express = require("express");
 const route = express.Router();
 const Controller = require("../controllers/Controller")
 
-route.get("/getgame", Controller.getGame)
+route.get("/getgame", Controller.getGame);
+route.get("/getgameby/:platform", Controller.getGameBy);
 
 route.use(["/login/admin", "/admin"], require("./adminRoutes"));
 route.use("/login/user",require("./userRoutes"));
--- backend/controllers/Controller.js
+++ backend/controllers/Controller.js
@@ -4,3 +4,14 @@ exports.getGame = async ( req, res ) => {
     const result = await Game.find({});
     res.send(result);
 }
+
+exports.getGameBy = async (req, res) => {
+    const platform = req.params.platform;
+    if (platform === "clear"){
+        return exports.getGame(req, res);
+    }
+    console.log(platform);
+    const result = await Game.find({ platforms: platform});
+    console.log(result);
+    res.send(result);
+}
