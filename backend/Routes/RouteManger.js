2771d69fac4e1e28331eb19b3ffd7fc35ff943f2
R-S-A-N-J-A-Y <sanjay638291@gmail.com>
2025-03-13 21:43:42 +0530

    feat: implement ordering functionality for game data retrieval

--- backend/Routes/RouteManger.js
+++ backend/Routes/RouteManger.js
@@ -3,7 +3,7 @@ const route = express.Router();
 const Controller = require("../controllers/Controller")
 
 route.get("/getgame", Controller.getGame);
-route.get("/getgameby/:platform", Controller.getGameBy);
+route.get("/getgameby", Controller.getGameBy);
 
 route.use(["/login/admin", "/admin"], require("./adminRoutes"));
 route.use("/login/user",require("./userRoutes"));
--- backend/controllers/Controller.js
+++ backend/controllers/Controller.js
@@ -5,8 +5,8 @@ exports.getGame = async ( req, res ) => {
     res.send(result);
 }
 
-exports.getGameBy = async (req, res) => {
-    const platform = req.params.platform;
+//Select By Platforms
+const getGameByPlatform = async (req, res, platform) => {
     if (platform === "clear"){
         return exports.getGame(req, res);
     }
@@ -14,4 +14,34 @@ exports.getGameBy = async (req, res) => {
     const result = await Game.find({ platforms: platform});
     console.log(result);
     res.send(result);
+};
+
+// Order By 
+const sortGameBy = async (req, res, orderBy) => {
+    if ( orderBy === "clear" )
+        return exports.getGame(req, res);
+    const result = await Game.find().sort({[orderBy]: 1})
+    console.log(result);
+    res.send(result);
+}
+
+//Select By Platforms and Order
+const getGameByPAS = async(req, res, orderBy, platform) => {
+    if ( orderBy === "clear" && platform === "clear" )
+        return exports.getGame(req, res);
+    else if ( orderBy === "clear" )
+        return getGameByPlatform(req, res, platform);
+    else if ( platform === "clear" )
+        return sortGameBy(req, res, orderBy);
+    const result = await Game.find({ platforms: platform }).sort({[orderBy] : 1});
+    return res.send(result);
+}
+
+exports.getGameBy = async (req, res) => {
+    const {platform, orderBy} = req.query;
+    if ( platform && orderBy )
+        return getGameByPAS(req, res, orderBy, platform);
+    else if ( platform )
+        return getGameByPlatform(req, res, platform);
+    return sortGameBy(req, res, orderBy);
 }
